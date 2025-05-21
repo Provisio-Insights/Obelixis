f00c6ce7a91a65ff32cca80fd6e152bb2f777b42
John Schieferle Uhlenbrock <johnsu@provisioinsights.com>
2025-05-21 10:42:57 -0500

    Add foundational module structure

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+mod config;
+mod file;
+mod process;
+mod system;
+mod network;
+mod services;
+mod output;
+
 fn main() {
     println!("Hello, world!");
+    config::init();
+    file::init();
+    process::init();
+    system::init();
+    network::init();
+    services::init();
+    output::init();
 }
